5b47656796bc10a1ebe7565757e2a1f825158ed5
Gláufer Charão <glaufer.charao@gmail.com>
2022-04-10 22:21:40 -0300

    database Seeding

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,11 +1,11 @@
 package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.dtos.CategoryDTO;
-import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.mapper.Mappable;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +15,7 @@ public class CategoryService implements Mappable {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Transactional(readOnly = true)
     public List<CategoryDTO> findAllCategory(){
         return map(categoryRepository.findAll(),CategoryDTO.class);
     }
